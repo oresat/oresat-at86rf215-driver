97a4c5b10e89d39550da8b4b19b089c5051be3df
Trenton-Ruf <truf@live.com>
2025-12-10 20:54:28 +0900

    add bulk register write example

--- examples/simple_radio.rs
+++ examples/simple_radio.rs
@@ -12,18 +12,31 @@ fn main() {
     radio.rf_cfg.value.set_irqp(true);
 
     let cmd = radio.rf_cfg.write_command();
-    println!("Write command: {:02X?}", cmd);
+    println!("Write command rf_cfg: {:02X?}", cmd);
 
     let cmd = radio.rf_cfg.read_command();
-    println!("Read command: {:02X?}", cmd);
+    println!("Read command rf_cfg: {:02X?}", cmd);
 
-    // Example of setting a field using an enum.
+    // Setting a field using an enum.
     radio.rf09_cmd.value.set_cmd(TransceiverCmd::Sleep);
 
     let cmd = radio.rf09_cmd.write_command();
-    println!("Write command: {:02X?}", cmd);
+    println!("Write command rf09_cmd: {:02X?}", cmd);
 
-    // Exmple of generating a read command for a large muli-register
+    // Generating a read command for a large muli-register
     let cmd = radio.bbc0_cnt.read_command();
-    println!("Read command: {:02X?}", cmd)
+    println!("Read command bbc0_cnt: {:02X?}", cmd);
+
+    // Bulk writes
+    let mut pending_writes = BulkWrites::new();
+
+    pending_writes.add(&mut radio.rf_cfg);
+    pending_writes.add(&mut radio.rf09_cmd);
+    pending_writes.add(&mut radio.rf_clko);
+
+    // This should generate 2 write commands since rf_cfg and rf_clko are contiguous registers.
+    let write_commands = pending_writes.generate_commands();
+    for (idx, command) in write_commands.into_iter().enumerate() {
+        println!("Bulk Write command [{}]: {:02X?}", idx, command)
+    }
 }
